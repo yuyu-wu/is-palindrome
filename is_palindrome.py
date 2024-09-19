c0f8e8add55829b20b41a3a42e00a16ae5273f08
yuyu <swharriet2012@gmail.com>
2024-09-19 13:43:47 +1000

    initial

---
+++ is_palindrome.py
@@ -0,0 +1,6 @@
+# is_palindrome
+# Write a function called is_palindrome. A Palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward. This function should take in one parameter and returns True or False depending on whether it is a palindrome. As a bonus, allow your function to ignore whitespace and capitalization so that is_palindrome('a man a plan a canal Panama') returns True.
+
+def is_palindrome(string):
+    stripped = string.replace(' ', '').lower()
+    return stripped == stripped[::-1]
